d3da76ad7468a63adf2f9c9136bdc1f405f7b8f7
Ezia <esia.belbachir@protonmail.com>
2017-07-05 16:14:00 +0200

    Complete last commit on Image class

--- src/main/java/util/datastructure/Image.java
+++ src/main/java/util/datastructure/Image.java
@@ -3,5 +3,22 @@ package util.datastructure;
 /**
  * Created by esia on 05/07/17.
  */
-public class Image {
+public abstract class Image {
+
+	public enum PixelFormat {
+		RGB
+	};
+
+	public final String file;
+
+	public Image(String file) {
+		this.file = file;
+	}
+
+	public abstract void load() throws ImageException;
+
+	public abstract boolean isLoaded();
+
+	public abstract int[] getData(PixelFormat format) throws ImageException;
+
 }
